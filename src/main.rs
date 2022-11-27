41d935e7ea08a1b2fdd29ee049a5be39f76391be
Chris Couzens <ccouzens@gmail.com>
2022-11-27 22:06:05 +0000

    Add all node types
    But not all there properties. I'll add as needed.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
-struct GlobalProperties {
-    id: String,
-    name: String,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
 struct Color {
     #[serde(rename = "r")]
     red: f64,
@@ -20,11 +13,17 @@ struct Color {
     alpha: f64,
 }
 
+fn default_true() -> bool {
+    true
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 struct Node {
-    #[serde(flatten)]
-    global_properties: GlobalProperties,
+    id: String,
+    name: String,
+    #[serde(default = "default_true")]
+    visible: bool,
     #[serde(flatten)]
     node_type: NodeType,
 }
@@ -40,6 +39,40 @@ enum NodeType {
         background_color: Color,
         children: Vec<Node>,
     },
+    #[serde(rename_all = "camelCase")]
+    Frame { children: Vec<Node> },
+    #[serde(rename_all = "camelCase")]
+    Group { children: Vec<Node> },
+    #[serde(rename_all = "camelCase")]
+    Vector,
+    #[serde(rename_all = "camelCase")]
+    BooleanOperation { children: Vec<Node> },
+    #[serde(rename_all = "camelCase")]
+    Star,
+    #[serde(rename_all = "camelCase")]
+    Line,
+    #[serde(rename_all = "camelCase")]
+    Ellipse,
+    #[serde(rename_all = "camelCase")]
+    RegularPolygon,
+    #[serde(rename_all = "camelCase")]
+    Rectangle,
+    #[serde(rename_all = "camelCase")]
+    Text { characters: String },
+    #[serde(rename_all = "camelCase")]
+    Slice,
+    #[serde(rename_all = "camelCase")]
+    Component { children: Vec<Node> },
+    #[serde(rename_all = "camelCase")]
+    ComponentSet { children: Vec<Node> },
+    #[serde(rename_all = "camelCase")]
+    Instance { children: Vec<Node> },
+    #[serde(rename_all = "camelCase")]
+    Sticky { characters: String },
+    #[serde(rename_all = "camelCase")]
+    ShapeWithText { characters: String },
+    #[serde(rename_all = "camelCase")]
+    Connector { characters: String },
     #[serde(other)]
     Unknown,
 }
@@ -47,8 +80,25 @@ enum NodeType {
 impl NodeType {
     fn children(&self) -> &[Node] {
         match self {
-            NodeType::Document { children } => &children,
+            NodeType::Document { children, .. } => &children,
             NodeType::Canvas { children, .. } => &children,
+            NodeType::Frame { children, .. } => &children,
+            NodeType::Group { children, .. } => &children,
+            NodeType::Vector => &[],
+            NodeType::BooleanOperation { children, .. } => &children,
+            NodeType::Star => &[],
+            NodeType::Line => &[],
+            NodeType::Ellipse => &[],
+            NodeType::RegularPolygon => &[],
+            NodeType::Rectangle => &[],
+            NodeType::Text { .. } => &[],
+            NodeType::Slice => &[],
+            NodeType::Component { children, .. } => &children,
+            NodeType::ComponentSet { children, .. } => &children,
+            NodeType::Instance { children, .. } => &children,
+            NodeType::Sticky { .. } => &[],
+            NodeType::ShapeWithText { .. } => &[],
+            NodeType::Connector { .. } => &[],
             NodeType::Unknown => &[],
         }
     }
