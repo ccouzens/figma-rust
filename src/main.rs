0b6774deb8e38ac0f92714dd83e6f45bb9b9f449
Chris Couzens <ccouzens@gmail.com>
2022-12-30 10:14:15 +0000

    Move motion logic to own file
    This should help avoid cluttering up main too much

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 mod figma_api;
+mod motion_tokens;
 use indexmap::IndexMap;
 use serde::Serialize;
-use serde_json::json;
 
-fn filter_node_by_prefix<'a>(prefixes: &'a [&'a str]) -> impl Fn(&&figma_api::Node) -> bool + 'a {
-    move |node: &&figma_api::Node| {
-        let node_prefix = node.name.split('/').next().unwrap_or_default().trim();
-        prefixes.contains(&node_prefix)
-    }
+fn node_match_prefix(prefixes: &[&str], node: &figma_api::Node) -> bool {
+    let node_prefix = node.name.split('/').next().unwrap_or_default().trim();
+    prefixes.contains(&node_prefix)
 }
 
 #[derive(Debug, Serialize)]
@@ -46,124 +44,8 @@ fn main() {
 
     let mut output = MapOrJson::Map(IndexMap::new());
 
-    for c in f
-        .document
-        .depth_first_iter()
-        .filter(filter_node_by_prefix(&["motion"]))
-    {
-        if let (Some(duration), Some(easing)) = (
-            c.frame_props().and_then(|f| f.transition_duration),
-            c.frame_props().and_then(|f| f.transition_easing.as_ref()),
-        ) {
-            let json = json!({
-                "category": "motion",
-                "exportKey": "motion",
-                "type": {
-                    "value": "scroll_animate",
-                    "type": "string"
-                },
-                "duration": {
-                    "value": duration / 1000.0,
-                    "type": "number",
-                    "unit": "s"
-                },
-                "easing": {
-                    "value": match easing {
-                        figma_api::EasingType::Linear => "linear",
-                        figma_api::EasingType::EaseIn => "ease-in",
-                        figma_api::EasingType::EaseOut => "ease-out",
-                        figma_api::EasingType::EaseInAndOut => "ease-in-out",
-                        figma_api::EasingType::EaseInBack => "ease-in-back",
-                        figma_api::EasingType::EaseOutBack => "ease-out-back",
-                        figma_api::EasingType::EaseInAndOutBack => "ease-in-out-back",
-                        figma_api::EasingType::CustomBezier => "custom-cubic-bezier",
-                        figma_api::EasingType::Gentle => "gentle",
-                        figma_api::EasingType::Quick => "quick",
-                        figma_api::EasingType::Bouncy => "bouncy",
-                        figma_api::EasingType::Slow => "slow",
-                        figma_api::EasingType::CustomSpring => "custom-spring",
-                    },
-                    "type": "string"
-                },
-                "easingFunction": match easing {
-                    figma_api::EasingType::Linear => json!({
-                        "x1": { "value": 0.0, "type": "number" },
-                        "x2": { "value": 1.0, "type": "number" },
-                        "y1": { "value": 0.0, "type": "number" },
-                        "y2": { "value": 1.0, "type": "number" }
-                    }),
-                    figma_api::EasingType::EaseIn => json!({
-                        "x1": { "value": 0.42, "type": "number" },
-                        "x2": { "value": 1.0, "type": "number" },
-                        "y1": { "value": 0.0, "type": "number" },
-                        "y2": { "value": 1.0, "type": "number" }
-                    }),
-                    figma_api::EasingType::EaseOut => json!({
-                        "x1": { "value": 0.0, "type": "number" },
-                        "x2": { "value": 0.58, "type": "number" },
-                        "y1": { "value": 0.0, "type": "number" },
-                        "y2": { "value": 1.0, "type": "number" }
-                    }),
-                    figma_api::EasingType::EaseInAndOut => json!({
-                        "x1": { "value": 0.42, "type": "number" },
-                        "x2": { "value": 0.58, "type": "number" },
-                        "y1": { "value": 0.0, "type": "number" },
-                        "y2": { "value": 1.0, "type": "number" }
-                    }),
-                    figma_api::EasingType::EaseInBack => json!({
-                        "x1": { "value": 0.3, "type": "number" },
-                        "x2": { "value": 0.7, "type": "number" },
-                        "y1": { "value": -0.05, "type": "number" },
-                        "y2": { "value": -0.5, "type": "number" }
-                    }),
-                    figma_api::EasingType::EaseOutBack => json!({
-                        "x1": { "value": 0.45, "type": "number" },
-                        "x2": { "value": 0.8, "type": "number" },
-                        "y1": { "value": 1.45, "type": "number" },
-                        "y2": { "value": 1.0, "type": "number" }
-                    }),
-                    figma_api::EasingType::EaseInAndOutBack => json!({
-                        "x1": { "value": 0.7, "type": "number" },
-                        "x2": { "value": 0.4, "type": "number" },
-                        "y1": { "value": -0.4, "type": "number" },
-                        "y2": { "value": 1.4, "type": "number" }
-                    }),
-                    // Ideally we'd read CustomBezier from the API, but it isn't provided so copy EaseInAndOut
-                    figma_api::EasingType::CustomBezier => json!({
-                        "x1": { "value": 0.42, "type": "number" },
-                        "x2": { "value": 0.58, "type": "number" },
-                        "y1": { "value": 0.0, "type": "number" },
-                        "y2": { "value": 1.0, "type": "number" }
-                    }),
-                    figma_api::EasingType::Gentle => json!({
-                        "mass": { "value": 1, "type": "number" },
-                        "stiffness": { "value": 100, "type": "number" },
-                        "damping": { "value": 15, "type": "number" }
-                    }),
-                    figma_api::EasingType::Quick => json!({
-                        "mass": { "value": 1, "type": "number" },
-                        "stiffness": { "value": 300, "type": "number" },
-                        "damping": { "value": 20, "type": "number" }
-                    }),
-                    figma_api::EasingType::Bouncy => json!({
-                        "mass": { "value": 1, "type": "number" },
-                        "stiffness": { "value": 600, "type": "number" },
-                        "damping": { "value": 15, "type": "number" }
-                    }),
-                    figma_api::EasingType::Slow => json!({
-                        "mass": { "value": 1, "type": "number" },
-                        "stiffness": { "value": 80, "type": "number" },
-                        "damping": { "value": 20, "type": "number" }
-                    }),
-                    // Ideally we'd read CustomSpring from the API, but it isn't provided so copy Gentle
-                    figma_api::EasingType::CustomSpring => json!({
-                        "mass": { "value": 1, "type": "number" },
-                        "stiffness": { "value": 100, "type": "number" },
-                        "damping": { "value": 15, "type": "number" }
-                    }),
-                }
-            });
-
+    for c in f.document.depth_first_iter() {
+        if let Some(json) = motion_tokens::motion_tokens_filter_map(c) {
             if !insert_by_name(&mut output, &c.name.split('/').collect::<Vec<_>>(), json) {
                 eprintln!("Failed to insert {}", &c.name);
             };
---
+++ src/motion_tokens.rs
@@ -0,0 +1,123 @@
+use serde_json::json;
+
+use crate::{
+    figma_api::{self, Node},
+    node_match_prefix,
+};
+
+pub fn motion_tokens_filter_map(node: &Node) -> Option<serde_json::Value> {
+    if !node_match_prefix(&["motion"], node) {
+        return None;
+    }
+    let frame_props = node.frame_props()?;
+    let duration = frame_props.transition_duration?;
+    let easing = frame_props.transition_easing.as_ref()?;
+    Some(json!({
+        "category": "motion",
+        "exportKey": "motion",
+        "type": {
+            "value": "scroll_animate",
+            "type": "string"
+        },
+        "duration": {
+            "value": duration / 1000.0,
+            "type": "number",
+            "unit": "s"
+        },
+        "easing": {
+            "value": match easing {
+                figma_api::EasingType::Linear => "linear",
+                figma_api::EasingType::EaseIn => "ease-in",
+                figma_api::EasingType::EaseOut => "ease-out",
+                figma_api::EasingType::EaseInAndOut => "ease-in-out",
+                figma_api::EasingType::EaseInBack => "ease-in-back",
+                figma_api::EasingType::EaseOutBack => "ease-out-back",
+                figma_api::EasingType::EaseInAndOutBack => "ease-in-out-back",
+                figma_api::EasingType::CustomBezier => "custom-cubic-bezier",
+                figma_api::EasingType::Gentle => "gentle",
+                figma_api::EasingType::Quick => "quick",
+                figma_api::EasingType::Bouncy => "bouncy",
+                figma_api::EasingType::Slow => "slow",
+                figma_api::EasingType::CustomSpring => "custom-spring",
+            },
+            "type": "string"
+        },
+        "easingFunction": match easing {
+            figma_api::EasingType::Linear => json!({
+                "x1": { "value": 0.0, "type": "number" },
+                "x2": { "value": 1.0, "type": "number" },
+                "y1": { "value": 0.0, "type": "number" },
+                "y2": { "value": 1.0, "type": "number" }
+            }),
+            figma_api::EasingType::EaseIn => json!({
+                "x1": { "value": 0.42, "type": "number" },
+                "x2": { "value": 1.0, "type": "number" },
+                "y1": { "value": 0.0, "type": "number" },
+                "y2": { "value": 1.0, "type": "number" }
+            }),
+            figma_api::EasingType::EaseOut => json!({
+                "x1": { "value": 0.0, "type": "number" },
+                "x2": { "value": 0.58, "type": "number" },
+                "y1": { "value": 0.0, "type": "number" },
+                "y2": { "value": 1.0, "type": "number" }
+            }),
+            figma_api::EasingType::EaseInAndOut => json!({
+                "x1": { "value": 0.42, "type": "number" },
+                "x2": { "value": 0.58, "type": "number" },
+                "y1": { "value": 0.0, "type": "number" },
+                "y2": { "value": 1.0, "type": "number" }
+            }),
+            figma_api::EasingType::EaseInBack => json!({
+                "x1": { "value": 0.3, "type": "number" },
+                "x2": { "value": 0.7, "type": "number" },
+                "y1": { "value": -0.05, "type": "number" },
+                "y2": { "value": -0.5, "type": "number" }
+            }),
+            figma_api::EasingType::EaseOutBack => json!({
+                "x1": { "value": 0.45, "type": "number" },
+                "x2": { "value": 0.8, "type": "number" },
+                "y1": { "value": 1.45, "type": "number" },
+                "y2": { "value": 1.0, "type": "number" }
+            }),
+            figma_api::EasingType::EaseInAndOutBack => json!({
+                "x1": { "value": 0.7, "type": "number" },
+                "x2": { "value": 0.4, "type": "number" },
+                "y1": { "value": -0.4, "type": "number" },
+                "y2": { "value": 1.4, "type": "number" }
+            }),
+            // Ideally we'd read CustomBezier from the API, but it isn't provided so copy EaseInAndOut
+            figma_api::EasingType::CustomBezier => json!({
+                "x1": { "value": 0.42, "type": "number" },
+                "x2": { "value": 0.58, "type": "number" },
+                "y1": { "value": 0.0, "type": "number" },
+                "y2": { "value": 1.0, "type": "number" }
+            }),
+            figma_api::EasingType::Gentle => json!({
+                "mass": { "value": 1, "type": "number" },
+                "stiffness": { "value": 100, "type": "number" },
+                "damping": { "value": 15, "type": "number" }
+            }),
+            figma_api::EasingType::Quick => json!({
+                "mass": { "value": 1, "type": "number" },
+                "stiffness": { "value": 300, "type": "number" },
+                "damping": { "value": 20, "type": "number" }
+            }),
+            figma_api::EasingType::Bouncy => json!({
+                "mass": { "value": 1, "type": "number" },
+                "stiffness": { "value": 600, "type": "number" },
+                "damping": { "value": 15, "type": "number" }
+            }),
+            figma_api::EasingType::Slow => json!({
+                "mass": { "value": 1, "type": "number" },
+                "stiffness": { "value": 80, "type": "number" },
+                "damping": { "value": 20, "type": "number" }
+            }),
+            // Ideally we'd read CustomSpring from the API, but it isn't provided so copy Gentle
+            figma_api::EasingType::CustomSpring => json!({
+                "mass": { "value": 1, "type": "number" },
+                "stiffness": { "value": 100, "type": "number" },
+                "damping": { "value": 15, "type": "number" }
+            }),
+        }
+    }))
+}
