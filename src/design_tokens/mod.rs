03fa02b32ca4b421339e4296520ed779794aadd5
Chris Couzens <ccouzens@gmail.com>
2023-01-02 12:40:29 +0000

    Start productionizing
    Add some error handling, and some command line parsing.
    
    At the moment the command line parsing is a bit pointless, but I hope to
    add other subcommands.

---
+++ src/design_tokens/mod.rs
@@ -0,0 +1,224 @@
+mod border_tokens;
+mod breakpoint_tokens;
+mod motion_tokens;
+mod opacity_tokens;
+mod radius_tokens;
+mod size_tokens;
+mod spacing_tokens;
+use super::figma_api;
+use anyhow::{Context, Result};
+use indexmap::IndexMap;
+use serde::Serialize;
+use serde_json::json;
+use std::{borrow::Cow, io::Write, iter::once};
+
+fn node_match_prefix(prefixes: &[&str], name: &str) -> bool {
+    let node_prefix = name.split('/').next().unwrap_or_default().trim();
+    prefixes.contains(&node_prefix)
+}
+
+#[derive(Debug, Serialize)]
+#[serde(untagged)]
+enum MapOrJson {
+    Map(IndexMap<String, MapOrJson>),
+    Json(serde_json::Value),
+}
+
+fn insert_by_name<'a>(output: &mut MapOrJson, name: &[&'a str], value: serde_json::Value) -> bool {
+    match output {
+        MapOrJson::Map(map) => {
+            let head = name[0].trim().to_lowercase();
+            let rest = &name[1..];
+            if rest.is_empty() {
+                match map.entry(head) {
+                    indexmap::map::Entry::Occupied(_) => false,
+                    indexmap::map::Entry::Vacant(vacancy) => {
+                        vacancy.insert(MapOrJson::Json(value));
+                        true
+                    }
+                }
+            } else {
+                let entry = map
+                    .entry(head)
+                    .or_insert_with(|| MapOrJson::Map(IndexMap::new()));
+                insert_by_name(entry, rest, value)
+            }
+        }
+        MapOrJson::Json(..) => false,
+    }
+}
+
+fn token_document_transformer(
+    file: &figma_api::File,
+    output: &mut MapOrJson,
+    prefixes: &[&str],
+    stderr: &mut impl Write,
+    transformer: impl Fn(&figma_api::Node, &figma_api::File) -> Option<serde_json::Value>,
+) {
+    for nodes in file.document.depth_first_stack_iter() {
+        let node = *nodes.last().unwrap();
+
+        let parent = nodes.iter().nth_back(1).cloned();
+        let name = match parent {
+            Some(figma_api::Node {
+                name: parent_name,
+                node_type: figma_api::NodeType::ComponentSet { .. },
+                ..
+            }) => Cow::Owned(
+                once(parent_name.as_str())
+                    .chain(
+                        node.name
+                            .split(',')
+                            .map(str::trim)
+                            .filter(|n| !n.starts_with('_') && !n.starts_with('.'))
+                            .filter_map(|n| n.split('=').nth(1))
+                            .map(str::trim),
+                    )
+                    .fold(String::new(), |mut acc, p| {
+                        if !acc.is_empty() {
+                            acc.push('/');
+                        }
+                        acc.push_str(p);
+                        acc
+                    }),
+            ),
+            _ => Cow::Borrowed(&node.name),
+        };
+        if !nodes
+            .iter()
+            .rev()
+            .skip(1)
+            .any(|n| n.name.split('/').next().map(str::trim) == Some("_tokens"))
+        {
+            continue;
+        }
+        if !node_match_prefix(prefixes, &name) {
+            continue;
+        }
+        if let Some(json) = transformer(node, file) {
+            if !insert_by_name(output, &name.split('/').collect::<Vec<_>>(), json) {
+                writeln!(stderr, "Failed to insert {}", name).unwrap();
+            };
+        }
+    }
+}
+
+fn token_style_transformer(
+    f: &figma_api::File,
+    output: &mut MapOrJson,
+    name: &str,
+    stderr: &mut impl Write,
+    style_type: figma_api::StyleType,
+) {
+    for style in f.styles.values() {
+        #[derive(Debug, Serialize)]
+        #[serde(rename_all = "camelCase")]
+        struct ExportToken<'a> {
+            category: &'a str,
+            export_key: &'a str,
+            #[serde(skip_serializing_if = "Option::is_none")]
+            comment: Option<&'a str>,
+        }
+
+        if style.style_type == style_type
+            && !style
+                .name
+                .trim_start()
+                .starts_with(|c| c == '.' || c == '_' || c == '*')
+        {
+            if !insert_by_name(
+                output,
+                &once(name).chain(style.name.split('/')).collect::<Vec<_>>(),
+                json!(ExportToken {
+                    category: name,
+                    export_key: name,
+                    comment: if style.description.is_empty() {
+                        None
+                    } else {
+                        Some(&style.description)
+                    },
+                }),
+            ) {
+                writeln!(stderr, "Failed to insert {}", name).unwrap();
+            };
+        }
+    }
+}
+
+pub fn main(
+    file: &figma_api::File,
+    stdout: &mut impl Write,
+    stderr: &mut impl Write,
+) -> Result<()> {
+    let mut output = MapOrJson::Map(IndexMap::new());
+
+    token_document_transformer(
+        &file,
+        &mut output,
+        &["size", "sizes"],
+        stderr,
+        size_tokens::as_size_token,
+    );
+    token_document_transformer(&file, &mut output, &["breakpoints"], stderr, |node, _| {
+        breakpoint_tokens::as_breakpoint_token(node, &file)
+    });
+    token_document_transformer(&file, &mut output, &["spacing"], stderr, |node, _| {
+        spacing_tokens::as_spacing_token(node, &file)
+    });
+    token_document_transformer(
+        &file,
+        &mut output,
+        &["borders", "border"],
+        stderr,
+        |node, _| border_tokens::as_border_token(node, &file),
+    );
+    token_document_transformer(
+        &file,
+        &mut output,
+        &["radius", "radii"],
+        stderr,
+        |node, _| radius_tokens::as_radius_token(node, &file),
+    );
+    token_document_transformer(
+        &file,
+        &mut output,
+        &["opacities", "opacity"],
+        stderr,
+        |node, _| opacity_tokens::as_opacity_token(node, &file),
+    );
+    token_document_transformer(&file, &mut output, &["motion"], stderr, |node, _| {
+        motion_tokens::as_motion_token(node)
+    });
+
+    token_style_transformer(
+        &file,
+        &mut output,
+        "color",
+        stderr,
+        figma_api::StyleType::Fill,
+    );
+    token_style_transformer(
+        &file,
+        &mut output,
+        "effect",
+        stderr,
+        figma_api::StyleType::Effect,
+    );
+    token_style_transformer(
+        &file,
+        &mut output,
+        "font",
+        stderr,
+        figma_api::StyleType::Text,
+    );
+    token_style_transformer(
+        &file,
+        &mut output,
+        "grid",
+        stderr,
+        figma_api::StyleType::Grid,
+    );
+
+    serde_json::to_writer_pretty(stdout, &output).context("Failed to write design tokens")?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,183 +1,37 @@
-mod border_tokens;
-mod breakpoint_tokens;
+mod design_tokens;
 mod figma_api;
-mod motion_tokens;
-mod opacity_tokens;
-mod radius_tokens;
-mod size_tokens;
-mod spacing_tokens;
-use std::{borrow::Cow, iter::once};
 
-use indexmap::IndexMap;
-use serde::Serialize;
-use serde_json::json;
+use anyhow::{Context, Result};
+use clap::{Parser, Subcommand};
 
-fn node_match_prefix(prefixes: &[&str], name: &str) -> bool {
-    let node_prefix = name.split('/').next().unwrap_or_default().trim();
-    prefixes.contains(&node_prefix)
+#[derive(Debug, Parser)]
+#[command(about = "Figma API tooling", long_about = Some("Figma API tooling. Requires a Figma file on stdin."))]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
 }
 
-#[derive(Debug, Serialize)]
-#[serde(untagged)]
-enum MapOrJson {
-    Map(IndexMap<String, MapOrJson>),
-    Json(serde_json::Value),
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(about = "Generate design tokens", long_about = Some("Generate design tokens. Not recommended due to limitations of the Figma API"))]
+    DesignTokens,
 }
 
-fn insert_by_name<'a>(output: &mut MapOrJson, name: &[&'a str], value: serde_json::Value) -> bool {
-    match output {
-        MapOrJson::Map(map) => {
-            let head = name[0].trim().to_lowercase();
-            let rest = &name[1..];
-            if rest.is_empty() {
-                match map.entry(head) {
-                    indexmap::map::Entry::Occupied(_) => false,
-                    indexmap::map::Entry::Vacant(vacancy) => {
-                        vacancy.insert(MapOrJson::Json(value));
-                        true
-                    }
-                }
-            } else {
-                let entry = map
-                    .entry(head)
-                    .or_insert_with(|| MapOrJson::Map(IndexMap::new()));
-                insert_by_name(entry, rest, value)
-            }
-        }
-        MapOrJson::Json(..) => false,
-    }
-}
+fn main() -> Result<()> {
+    let args = Cli::parse();
 
-fn token_document_transformer(
-    file: &figma_api::File,
-    output: &mut MapOrJson,
-    prefixes: &[&str],
-    transformer: impl Fn(&figma_api::Node, &figma_api::File) -> Option<serde_json::Value>,
-) {
-    for nodes in file.document.depth_first_stack_iter() {
-        let node = *nodes.last().unwrap();
+    let file: figma_api::File = serde_json::from_reader(std::io::stdin())
+        .context("Failed to parse Figma API file from stdin")?;
 
-        let parent = nodes.iter().nth_back(1).cloned();
-        let name = match parent {
-            Some(figma_api::Node {
-                name: parent_name,
-                node_type: figma_api::NodeType::ComponentSet { .. },
-                ..
-            }) => Cow::Owned(
-                once(parent_name.as_str())
-                    .chain(
-                        node.name
-                            .split(',')
-                            .map(str::trim)
-                            .filter(|n| !n.starts_with('_') && !n.starts_with('.'))
-                            .filter_map(|n| n.split('=').nth(1))
-                            .map(str::trim),
-                    )
-                    .fold(String::new(), |mut acc, p| {
-                        if !acc.is_empty() {
-                            acc.push('/');
-                        }
-                        acc.push_str(p);
-                        acc
-                    }),
-            ),
-            _ => Cow::Borrowed(&node.name),
-        };
-        if !nodes
-            .iter()
-            .rev()
-            .skip(1)
-            .any(|n| n.name.split('/').next().map(str::trim) == Some("_tokens"))
-        {
-            continue;
-        }
-        if !node_match_prefix(prefixes, &name) {
-            continue;
-        }
-        if let Some(json) = transformer(node, file) {
-            if !insert_by_name(output, &name.split('/').collect::<Vec<_>>(), json) {
-                eprintln!("Failed to insert {}", name);
-            };
+    match &args.command {
+        Commands::DesignTokens => {
+            design_tokens::main(
+                &file,
+                &mut std::io::stdout().lock(),
+                &mut std::io::stderr().lock(),
+            )
+            .context("Failed to generate design tokens")?;
         }
     }
-}
-
-fn token_style_transformer(
-    f: &figma_api::File,
-    output: &mut MapOrJson,
-    name: &str,
-    style_type: figma_api::StyleType,
-) {
-    for style in f.styles.values() {
-        #[derive(Debug, Serialize)]
-        #[serde(rename_all = "camelCase")]
-        struct ExportToken<'a> {
-            category: &'a str,
-            export_key: &'a str,
-            #[serde(skip_serializing_if = "Option::is_none")]
-            comment: Option<&'a str>,
-        }
-
-        if style.style_type == style_type
-            && !style
-                .name
-                .trim_start()
-                .starts_with(|c| c == '.' || c == '_' || c == '*')
-        {
-            insert_by_name(
-                output,
-                &once(name).chain(style.name.split('/')).collect::<Vec<_>>(),
-                json!(ExportToken {
-                    category: name,
-                    export_key: name,
-                    comment: if style.description.is_empty() {
-                        None
-                    } else {
-                        Some(&style.description)
-                    },
-                }),
-            );
-        }
-    }
-}
-
-fn main() {
-    let f: figma_api::File = serde_json::from_reader(std::io::stdin()).unwrap();
-
-    let mut output = MapOrJson::Map(IndexMap::new());
-
-    token_document_transformer(
-        &f,
-        &mut output,
-        &["size", "sizes"],
-        size_tokens::as_size_token,
-    );
-    token_document_transformer(&f, &mut output, &["breakpoints"], |node, _| {
-        breakpoint_tokens::as_breakpoint_token(node, &f)
-    });
-    token_document_transformer(&f, &mut output, &["spacing"], |node, _| {
-        spacing_tokens::as_spacing_token(node, &f)
-    });
-    token_document_transformer(&f, &mut output, &["borders", "border"], |node, _| {
-        border_tokens::as_border_token(node, &f)
-    });
-    token_document_transformer(&f, &mut output, &["radius", "radii"], |node, _| {
-        radius_tokens::as_radius_token(node, &f)
-    });
-    token_document_transformer(&f, &mut output, &["opacities", "opacity"], |node, _| {
-        opacity_tokens::as_opacity_token(node, &f)
-    });
-    token_document_transformer(&f, &mut output, &["motion"], |node, _| {
-        motion_tokens::as_motion_token(node)
-    });
-
-    token_style_transformer(&f, &mut output, "color", figma_api::StyleType::Fill);
-    token_style_transformer(&f, &mut output, "effect", figma_api::StyleType::Effect);
-    token_style_transformer(&f, &mut output, "font", figma_api::StyleType::Text);
-    token_style_transformer(&f, &mut output, "grid", figma_api::StyleType::Grid);
-
-    println!(
-        "{}",
-        serde_json::to_string_pretty(&output).unwrap_or_else(|_| "Err".into())
-    )
+    Ok(())
 }
