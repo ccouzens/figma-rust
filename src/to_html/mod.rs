3d1557002a99bbc95455234b59800e42958f0109
Chris Couzens <ccouzens@gmail.com>
2023-03-11 14:43:34 +0000

    Get more body css properties from the file
    I might change how this works so that instead of erroring on a missing
    property, we leave off that bit of CSS

--- src/to_html/mod.rs
+++ src/to_html/mod.rs
@@ -22,7 +22,37 @@ pub fn main(
         .context("Expected to have a background fill")?
         .color()
         .context("Expected to have a background color")?;
-    // serde_json::to_writer_pretty(stdout, &node).context("Failed to write node")?;
+
+    let node_bounding_box = node
+        .absolute_bounding_box()
+        .context("Expected to have a render box")?;
+
+    let body_css = format!(
+        "
+    box-sizing: border-box;
+    position: relative;
+    width: {width}px;
+    height: {height}px;
+    background: {background_color};
+    border: 1px dashed {border_color};
+    border-radius: {border_radius}px;
+   ",
+        width = node_bounding_box
+            .width
+            .context("Expected to have a width")?,
+        height = node_bounding_box
+            .height
+            .context("Expected to have a height")?,
+        background_color = node_background_color.to_rgb_string(),
+        border_color = node
+            .strokes()
+            .get(0)
+            .context("Expected to have a border stroke")?
+            .color()
+            .context("Expected to have a border color")?
+            .to_rgb_string(),
+        border_radius = node.corner_radius().context("Expected a border radius")?
+    );
 
     writeln!(
         stdout,
@@ -44,6 +74,7 @@ pub fn main(
             border: 1px dashed #7b61ff;
             border-radius: 5px;
           ", node_background_color.to_rgb_string())) {}
+                body(style=&body_css) {}
             }
         }
     )
