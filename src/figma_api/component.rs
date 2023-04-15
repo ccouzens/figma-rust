bad25a324d8a53fb530e1dd24556918d73735e7a
Chris Couzens <ccouzens@gmail.com>
2023-04-15 21:26:19 +0100

    Add additional exports

--- src/figma_api/component.rs
+++ src/figma_api/component.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 /// [Figma documentation](https://www.figma.com/developers/api#component-type)
 #[derive(Debug, Deserialize, Serialize)]
+#[typeshare::typeshare]
 pub struct Component {
     pub key: String,
     pub name: String,
