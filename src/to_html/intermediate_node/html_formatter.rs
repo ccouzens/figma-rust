4b4899aafbc6c683dc0c715e8e5dfd3dae9bc301
Chris Couzens <ccouzens@gmail.com>
2023-06-03 16:11:23 +0100

    Start adding common attributes to the nodes
    Next step will be styles!

--- src/to_html/intermediate_node/html_formatter.rs
+++ src/to_html/intermediate_node/html_formatter.rs
@@ -46,11 +46,28 @@ fn end_tag(f: &mut std::fmt::Formatter<'_>, level: u16, name: &str) -> std::fmt:
     write!(f, ">")
 }
 
+fn common_attributes(
+    f: &mut std::fmt::Formatter<'_>,
+    level: u16,
+    intermediate_node: &IntermediateNode<'_>,
+) -> std::fmt::Result {
+    attribute(f, level, "data-figma-name", intermediate_node.figma.name)?;
+    attribute(f, level, "data-figma-id", intermediate_node.figma.id)?;
+    attribute(
+        f,
+        level,
+        "data-figma-type",
+        &format!("{:?}", intermediate_node.figma.r#type),
+    )?;
+    Ok(())
+}
+
 impl<'a> Display for HtmlFormatter<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self.intermediate_node.node_type {
             IntermediateNodeType::Vector => {
                 open_start_tag(f, self.nesting_depth, "svg")?;
+                common_attributes(f, self.nesting_depth, self.intermediate_node)?;
                 attribute(f, self.nesting_depth, "viewBox", "-5 -81 100 100")?;
                 close_start_tag(f, self.nesting_depth)?;
                 open_start_tag(f, self.nesting_depth + 1, "text")?;
@@ -62,12 +79,14 @@ impl<'a> Display for HtmlFormatter<'a> {
             }
             IntermediateNodeType::Text { text: inner_text } => {
                 open_start_tag(f, self.nesting_depth, "div")?;
+                common_attributes(f, self.nesting_depth, self.intermediate_node)?;
                 close_start_tag(f, self.nesting_depth)?;
                 text(f, self.nesting_depth, inner_text)?;
                 end_tag(f, self.nesting_depth, "div")?;
             }
             IntermediateNodeType::Frame { children } => {
                 open_start_tag(f, self.nesting_depth, "div")?;
+                common_attributes(f, self.nesting_depth, self.intermediate_node)?;
                 close_start_tag(f, self.nesting_depth)?;
                 for child in children.iter() {
                     write!(
